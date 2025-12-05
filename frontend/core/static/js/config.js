111d5fb6d416604cf3038a91e0273b266b0393fd
Gustavo Sganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2025-12-05 11:50:43 -0400

    fixed api endpoints to avoid conflict with /predict from django

--- frontend/core/static/js/config.js
+++ frontend/core/static/js/config.js
@@ -3,6 +3,6 @@ const CONFIG = {
     // If running locally (localhost), use Flask dev port
     API_URL: window.location.hostname === "localhost"
              ? "http://localhost:5001/predict"
-             : "/predict"   // On deployment, Nginx will forward /predict to Flask
+             : "/api/predict"   // On deployment, Nginx will forward /predict to Flask
   };
   

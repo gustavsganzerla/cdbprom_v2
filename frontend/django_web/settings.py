0a9df6465e3564a6df11caa873ee60dab23f7734
Gustavo Sganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2025-12-12 13:03:50 -0400

    env variables

--- frontend/django_web/settings.py
+++ frontend/django_web/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jz=bp*5+@#mp%ll6dkn5xglpq+2i1%tc54s_xhvdhke6+u*#-&'
+SECRET_KEY = os.environ.get("DJANGOKEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
